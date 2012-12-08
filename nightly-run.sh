7ab1bf0a5cf42552cb2d306ce25e0b6fabb03c8b
root <root@oahu.fglan>
2012-12-08 12:14:18 -0800

    Add required cronjobs for actually syncing the data

--- nightly-run.sh
+++ nightly-run.sh
@@ -2,7 +2,7 @@
 
 # nightly run to update allowed_list and resync all repositories
 
-ssh -i ~/.ssh/scp_key dev.freegeek.org ls /git > /git/allowed_list
+ssh -i ~ryan52/.ssh/scp_key ryan52@dev.freegeek.org ls /git > /git/allowed_list
 for i in $(cat /git/allowed_list | grep -v ^distro$); do
     sudo su - www-data -c "/home/mirror-daemon/fg-git-tools/request-mirror-update git $i"
 done
